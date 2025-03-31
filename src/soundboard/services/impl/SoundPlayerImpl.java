c907460e2c67e91a84c9acb60d7a173b8c10223f
Gitmoid <vrana.vojtech@post.cz>
2025-03-31 13:30:50 +0200

    fixed that multiple songs could play at the same time because of the delay

--- src/soundboard/services/impl/SoundPlayerImpl.java
+++ src/soundboard/services/impl/SoundPlayerImpl.java
@@ -5,8 +5,10 @@ import soundboard.services.MP3Player;
 import javax.swing.SwingWorker;
 
 public class SoundPlayerImpl {
-    private SwingWorker<Void, Void> currentPlayingWorker = null;
     private final MP3Player mp3Player;
+
+    private SwingWorker<Void, Void> currentPlayingWorker = null;
+    private boolean isPlaying = false;
 
     public SoundPlayerImpl() {
         this.mp3Player = new MP3Player();
@@ -15,25 +17,44 @@ public class SoundPlayerImpl {
     public synchronized void playSound(String filePath) {
         int playbackDelayInMilliseconds = 500;
         stopSound();
+
         currentPlayingWorker = new SwingWorker<>() {
             @Override
             protected Void doInBackground() {
                 try {
                     Thread.sleep(playbackDelayInMilliseconds);
                 } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    Thread.currentThread().interrupt();
+                    return null;
                 }
-                mp3Player.play(filePath);
+                if (!isCancelled()) {
+                    isPlaying = true;
+                    mp3Player.play(filePath);
+                }
                 return null;
+            }
+
+            @Override
+            protected void done() {
+                isPlaying = false;
+                currentPlayingWorker = null;
             }
         };
 
         currentPlayingWorker.execute();
     }
 
-    public void stopSound() {
-        if (currentPlayingWorker != null && !currentPlayingWorker.isDone()) {
+    public synchronized void stopSound() {
+        // First, stop any currently playing sound via the MP3Player
+        if (isPlaying) {
             mp3Player.stop();
+            isPlaying = false;
+        }
+
+        // Then, cancel any pending SwingWorker that might be in the delay phase
+        if (currentPlayingWorker != null && !currentPlayingWorker.isDone()) {
+            currentPlayingWorker.cancel(true);
+            currentPlayingWorker = null;
         }
     }
 }
